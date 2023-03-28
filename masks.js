854ca23b29aab05f81b078d46e9c7d21499f6d8c
Felipe Gomes <fgomesdeluna@gmail.com>
2023-03-28 17:02:27 -0300

    Update masks.js

--- masks.js
+++ masks.js
@@ -1,98 +1,96 @@
-const masks = {
-  cpf (value) {
-    return value
-      .replace(/\D+/g, '')
-      .replace(/(\d{3})(\d)/, '$1.$2')
-      .replace(/(\d{3})(\d)/, '$1.$2')
-      .replace(/(\d{3})(\d{1,2})/, '$1-$2')
-      .replace(/(-\d{2})\d+?$/, '$1')
-  },
+export const money = (value) => {
+  const cleanValue = +value.replace(/\D+/g, "");
+  const options = { style: "currency", currency: "BRL" };
+  return new Intl.NumberFormat("pt-br", options).format(cleanValue / 100);
+};
 
-  cnpj (value) {
-    return value
-      .replace(/\D+/g, '')
-      .replace(/(\d{2})(\d)/, '$1.$2')
-      .replace(/(\d{3})(\d)/, '$1.$2')
-      .replace(/(\d{3})(\d)/, '$1/$2')
-      .replace(/(\d{4})(\d)/, '$1-$2')
-      .replace(/(-\d{2})\d+?$/, '$1')
-  },
+export const cpf = (value) => {
+  return value
+    .replace(/\D+/g, "")
+    .replace(/(\d{3})(\d)/, "$1.$2")
+    .replace(/(\d{3})(\d)/, "$1.$2")
+    .replace(/(\d{3})(\d{1,2})/, "$1-$2")
+    .replace(/(-\d{2})\d+?$/, "$1");
+};
 
-  phone (value) {
-    return value
-      .replace(/\D+/g, '')
-      .replace(/(\d{2})(\d)/, '($1) $2')
-      .replace(/(\d{4})(\d)/, '$1-$2')
-      .replace(/(\d{4})-(\d)(\d{4})/, '$1$2-$3')
-      .replace(/(-\d{4})\d+?$/, '$1')
-  },
+export const cnpj = (value) => {
+  return value
+    .replace(/\D+/g, "")
+    .replace(/(\d{2})(\d)/, "$1.$2")
+    .replace(/(\d{3})(\d)/, "$1.$2")
+    .replace(/(\d{3})(\d)/, "$1/$2")
+    .replace(/(\d{4})(\d)/, "$1-$2")
+    .replace(/(-\d{2})\d+?$/, "$1");
+};
 
-  phoneDDI (value) {
-    return value
-      .replace(/\D+/g, '')
-      .replace(/(\d{2})(\d)/, '+$1 $2')
-      .replace(/(\d{2})(\d)/, '($1) $2')
-      .replace(/(\d{4})(\d)/, '$1-$2')
-      .replace(/(\d{4})-(\d)(\d{4})/, '$1$2-$3')
-      .replace(/(-\d{4})\d+?$/, '$1')
-  },
+export const cpfCnpjMask = (value) => {
+  if (value.length < 15) {
+    return cpfMask(value);
+  } else {
+    return cnpjMask(value);
+  }
+};
 
-  cep (value) {
-    return value
-      .replace(/\D+/g, '')
-      .replace(/(\d{5})(\d)/, '$1-$2')
-      .replace(/(-\d{3})\d+?$/, '$1')
-  },
+export const phone = (value) => {
+  return value
+    .replace(/\D+/g, "")
+    .replace(/(\d{2})(\d)/, "($1) $2")
+    .replace(/(\d{4})(\d)/, "$1-$2")
+    .replace(/(\d{4})-(\d)(\d{4})/, "$1$2-$3")
+    .replace(/(-\d{4})\d+?$/, "$1");
+};
 
-  pis (value) {
-    return value
-      .replace(/\D+/g, '')
-      .replace(/(\d{3})(\d)/, '$1.$2')
-      .replace(/(\d{5})(\d)/, '$1.$2')
-      .replace(/(\d{5}\.)(\d{2})(\d)/, '$1$2-$3')
-      .replace(/(-\d)\d+?$/, '$1')
-  },
+export const phoneDDI = (value) => {
+  return value
+    .replace(/\D+/g, "")
+    .replace(/(\d{2})(\d)/, "+$1 $2")
+    .replace(/(\d{2})(\d)/, "($1) $2")
+    .replace(/(\d{4})(\d)/, "$1-$2")
+    .replace(/(\d{4})-(\d)(\d{4})/, "$1$2-$3")
+    .replace(/(-\d{4})\d+?$/, "$1");
+};
 
-  money (value) {
-    const cleanValue = +value.replace(/\D+/g, '')
-    const options = { style: 'currency', currency: 'BRL' }
-    return new Intl.NumberFormat('pt-br', options).format(cleanValue / 100)
-  },
+export const cep = (value) => {
+  return value
+    .replace(/\D+/g, "")
+    .replace(/(\d{5})(\d)/, "$1-$2")
+    .replace(/(-\d{3})\d+?$/, "$1");
+};
 
-  date (value) {
-    return value
-      .replace(/\D+/g, '')
-      .replace(/(\d{2})(\d)/, '$1/$2')
-      .replace(/(\/\d{2})(\d)/, '$1/$2')
-      .replace(/(\/\d{4})\d+?$/, '$1')
-  },
+export const pis = (value) => {
+  return value
+    .replace(/\D+/g, "")
+    .replace(/(\d{3})(\d)/, "$1.$2")
+    .replace(/(\d{5})(\d)/, "$1.$2")
+    .replace(/(\d{5}\.)(\d{2})(\d)/, "$1$2-$3")
+    .replace(/(-\d)\d+?$/, "$1");
+};
 
-  dateWithDashes (value) {
-    return value
-      .replace(/\D+/g, '')
-      .replace(/(\d{2})(\d)/, '$1-$2')
-      .replace(/(-\d{2})(\d)/, '$1-$2')
-      .replace(/(-\d{4})\d+?$/, '$1')
-  },
+export const date = (value) => {
+  return value
+    .replace(/\D+/g, "")
+    .replace(/(\d{2})(\d)/, "$1/$2")
+    .replace(/(\/\d{2})(\d)/, "$1/$2")
+    .replace(/(\/\d{4})\d+?$/, "$1");
+};
 
-  hour (value) {
-    return value
-      .replace(/\D+/g, '')
-      .replace(/(\d{2})(\d)/, '$1:$2')
-      .replace(/(:\d{2})\d+?$/, '$1')
-  },
-}
+export const dateWithDashes = (value) => {
+  return value
+    .replace(/\D+/g, "")
+    .replace(/(\d{2})(\d)/, "$1-$2")
+    .replace(/(-\d{2})(\d)/, "$1-$2")
+    .replace(/(-\d{4})\d+?$/, "$1");
+};
 
-document.querySelectorAll('input').forEach($input => {
-  const field = $input.dataset.js
-
-  $input.addEventListener('input', e => {
-    e.target.value = masks[field](e.target.value)
-  }, false)
-})
+export const hour = (value) => {
+  return value
+    .replace(/\D+/g, "")
+    .replace(/(\d{2})(\d)/, "$1:$2")
+    .replace(/(:\d{2})\d+?$/, "$1");
+};
 
 // separados com espaço
-export const cardMask = (value: any) => {
+export const cardMaskSpace = (value) => {
   return value
     .replace(/\D/g, "")
     .replace(/(\d{4})(\d)/, "$1 $2")
@@ -102,7 +100,7 @@ export const cardMask = (value: any) => {
 };
 
 // separados com traço
-export const cardMask = (value: any) => {
+export const cardMaskDashes = (value) => {
   return value
     .replace(/\D/g, "")
     .replace(/(\d{4})(\d)/, "$1-$2")
@@ -110,4 +108,3 @@ export const cardMask = (value: any) => {
     .replace(/(\d{4})(\d)/, "$1-$2")
     .replace(/(-\d{4})\d+?$/, "$1");
 };
-
